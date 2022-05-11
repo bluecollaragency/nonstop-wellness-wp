03c86cd9f46624c198f23e31b08829486ce270f4
Ian Daly <ineptian@gmail.com>
2022-05-11 12:00:25 -0700

    fixed what i wanted to in the main nav

--- template-parts/partials/main-nav.php
+++ template-parts/partials/main-nav.php
@@ -4,10 +4,14 @@
 
 ?>
 <div class="relative w-full text-dark bg-light items-center flex">
-	<div x-data="{ open: false }" class="relative flex flex-col w-full max-w-screen-2xl mx-auto md:items-center md:justify-start md:flex-row">
+	<div 
+    x-data="{ open: false }" 
+    x-effect="document.body.classList.toggle('mobile-nav-open', open)" 
+    class="relative flex flex-col w-full max-w-screen-2xl mx-auto md:items-center md:justify-start md:flex-row"
+    >
 		<div class="relative p-4 flex flex-row items-center justify-between h-[112px]">
 			<a href="<?= home_url('/'); ?>" class="text-lg font-semibold tracking-widest text-gray-900 uppercase rounded-lg dark-mode:text-white focus:outline-none focus:shadow-outline" rel="home" aria-current="page">Flowtrail UI</a>
-			<button class="md:hidden rounded-lg focus:outline-none focus:shadow-outline" @click="open = !open; mobNavOpen = !mobNavOpen" @keyup.escape.window="open = false; mobNavOpen = false">
+			<button class="md:hidden rounded-lg focus:outline-none focus:shadow-outline" @click="open = !open" @keyup.escape.window="open = false">
 				<svg fill="currentColor" viewBox="0 0 20 20" class="w-6 h-6">
 					<path x-show="!open" fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z" clip-rule="evenodd"></path>
 					<path x-show="open" fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
