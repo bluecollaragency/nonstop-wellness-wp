b099ed3a7b2f9c3a56039017a8417fc40ff84185
Ian Daly <ineptian@gmail.com>
2022-05-26 22:31:39 -0700

    starting to style select

--- page_full-width.php
+++ page_full-width.php
@@ -4,6 +4,14 @@
 
 get_header(); ?>
 
+  <div class="max-w-screen-sm mx-auto px-8 lg:px-4">
+    <select name="" id="" class="w-full rounded-2xl border-none outline-none focus:ring-gray-300 mt-1 px-4 py-3">
+      <option value="1">Name</option>
+      <option value="2">Something else</option>
+      <option value="3">Another thing</option>
+    </select>
+  </div>
+
 	<?php if ( have_posts() ) : ?>
 		<?php
 		while ( have_posts() ) :
@@ -15,7 +23,6 @@ get_header(); ?>
 		<?php endwhile; ?>
 
 	<?php endif; ?>
-
 
 <?php
 get_footer();
--- tailwind.config.js
+++ tailwind.config.js
@@ -52,6 +52,7 @@ module.exports = {
         }
     },
     plugins: [
-        tailpress.tailwind
+        tailpress.tailwind,
+        require('@tailwindcss/forms')
     ]
 };
