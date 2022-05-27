08b2a06273fd1558fb7030241eaac07a5245abdc
Ian Daly <ineptian@gmail.com>
2022-05-27 09:32:34 -0700

    working little select menu with reveal functionality

--- page_full-width.php
+++ page_full-width.php
@@ -5,11 +5,39 @@
 get_header(); ?>
 
   <div class="max-w-screen-sm mx-auto px-8 lg:px-4">
-    <select name="" id="" class="w-full rounded-2xl border-none outline-none focus:ring-gray-300 mt-1 px-4 py-3">
-      <option value="1">Name</option>
-      <option value="2">Something else</option>
-      <option value="3">Another thing</option>
-    </select>
+    <div x-data="{ formselect: '' }">
+    	<select 
+				name="" 
+				id="" 
+				class="w-full rounded-2xl border-none outline-none focus:ring-gray-300 mt-1 px-4 py-3"
+				x-model="formselect"
+				>
+				<option value="" disabled>Please select a reason for contacting us</option>
+	      <option>Name</option>
+	      <option>Something else</option>
+	      <option>Another thing</option>
+	    </select>
+<!-- 	
+			<div>
+				selected: <span x-text="formselect"></span>
+			</div> -->
+			<div 
+				x-show="formselect === 'Name'"
+				>
+				name
+			</div>
+			<div 
+				x-show="formselect === 'Something else'"
+				>
+				something else
+			</div>
+			<div 
+				x-show="formselect === 'Another thing'"
+				>
+				another thing
+			</div>
+
+    </div>
   </div>
 
 	<?php if ( have_posts() ) : ?>
