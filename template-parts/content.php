20e2183c7f46d131d540f457ffe7e85ed225cd4a
Ian Daly <ineptian@gmail.com>
2022-05-31 14:57:15 -0700

    added image to posts on archive page

--- template-parts/content.php
+++ template-parts/content.php
@@ -1,27 +1,28 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'mb-12 bg-white rounded-4xl' ); ?>>
 
-	
 	<div class="relative w-full">
 		<div class="w-full h-0 pb-[100%]">
-		<?php if( has_post_thumbnail() ){
-			echo the_post_thumbnail('medium', array('class' => 'absolute w-full h-full object-cover rounded-t-4xl') );
-		} else {
-			echo '<img 
-			src="https://via.placeholder.com/822x822" 
-			alt="Placeholder Image"
-			class="absolute w-full h-full object-cover rounded-t-4xl"
-		>';
-		} ?>
+			<a href="<?= esc_url( get_permalink() ); ?>">
+			<?php if( has_post_thumbnail() ){
+				echo the_post_thumbnail('medium', array('class' => 'absolute w-full h-full object-cover rounded-t-4xl') );
+			} else {
+				echo '<img 
+					src="https://via.placeholder.com/822x822" 
+					alt="Placeholder Image"
+					class="absolute w-full h-full object-cover rounded-t-4xl"
+				>';
+			} ?>
+			</a>
 		</div>
 	</div>
 	<div class="bg-white p-10 rounded-b-4xl">
 		<header class="entry-header">
-			<?php the_title( sprintf( '<h2 class="entry-title text-2xl md:text-3xl font-extrabold leading-tight mb-1"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+			<?php the_title( sprintf( '<h2 class="entry-title text-2xl md:text-3xl font-extrabold leading-tight mb-1 text-dark"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 		</header>
 
 		<?php if ( is_search() || is_archive() ) : ?>
 
-			<div class="entry-summary mt-6">
+			<div class="entry-summary mt-6 text-gray-400">
 				<?php the_excerpt(); ?>
 			</div>
 
