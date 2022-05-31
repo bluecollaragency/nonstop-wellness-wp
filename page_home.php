89513bbc2c3b833dd8d24a74da8d2d860fc7048c
Ian Daly <ineptian@gmail.com>
2022-05-31 13:16:37 -0700

    adding icon overlay for homepage image text sections

--- page_home.php
+++ page_home.php
@@ -283,7 +283,7 @@ get_header(); ?>
 		</div>
 	</div>
 	<div class="container mx-auto px-8 lg:px-4">
-		<div class="grid grid-cols-12 mt-4 gap-y-16 gap-x-6 relative">
+		<div class="grid lg:grid-cols-12 mt-4 gap-y-16 gap-x-6 relative">
 			<div class="col-start-1 xl:col-start-2 col-end-12 lg:col-end-6 overflow-visible">
 				<p class="">The Nonstop team takes immense pride in our level of customer service and the ability to develop strong trusting relationships with our clients.</p>
 				<a href="#" class="btn-lg bg-dark text-white mt-6">Learn about us</a>
@@ -291,7 +291,14 @@ get_header(); ?>
 				<div class="absolute w-1/2 h-[2px] bg-primary hidden lg:flex items-center z-20 mt-4 after:right-0 after:absolute after:h-4 after:w-4 after:border-t-2 after:border-r-2 after:transform after:rotate-45 after:border-primary"></div>
 			</div>
 			<div class="relative col-start-1 lg:col-start-7 col-end-12 md:col-end-12">
-				<img src="//via.placeholder.com/520x520" alt="" class="mx-auto rounded-4xl w-full h-auto">
+				<div class="relative">
+					<img src="//via.placeholder.com/520x520" alt="" class="mx-auto rounded-4xl w-full h-auto">
+					<img 
+						src="https://via.placeholder.com/94x94" 
+						alt="Placeholder Image"
+						class="absolute right-6 -bottom-6 w-1/3 lg:bottom-12 lg:-right-12"
+					>
+				</div>
 				<p class="mt-8 block lg:hidden ml-12">A healthy community is a thriving one. Early access and preventative care is key to driving the wellbeing of our communities. Everyone deserves equal access to comprehensive health coverage.</p>			
 				<div class="absolute h-1/2 bottom-0 w-[2px] bg-primary flex lg:hidden z-20 ml-7 after:mx-auto after:top-0 after:absolute after:h-3 after:w-3 after:border-t-2 after:border-l-2 after:transform after:rotate-45 after:border-primary after:-ml-[5px]"></div>
 			</div>
@@ -303,6 +310,11 @@ get_header(); ?>
 	<div class="grid lg:grid-cols-12 mt-4 gap-y-8 gap-x-6 items-center relative">
 		<div class="relative col-start-1 xl:col-start-2 col-end-12 lg:col-end-7 overflow-visible">
 			<img src="//via.placeholder.com/520x520" alt="" class="mx-auto rounded-4xl w-full h-auto">
+			<img 
+				src="https://via.placeholder.com/94x94" 
+				alt="Placeholder Image"
+				class="absolute right-6 -bottom-6 w-1/3 lg:bottom-12 lg:-left-12"
+			>
 		</div>
 		<div class="col-start-1 lg:col-start-8 xl:col-start-8 col-end-12 md:col-end-12 lg:col-span-5 xl:col-span-4">
 			<p class="lg:mt-12 ml-12 lg:ml-0">A healthy community is a thriving one. Early access and preventative care is key to driving the wellbeing of our communities. Everyone deserves equal access to comprehensive health coverage.</p>
@@ -345,7 +357,6 @@ get_header(); ?>
 
 <div class="container mx-auto px-8 lg:px-4 mt-24">
 	<div class="grid lg:grid-cols-12 xl:grid-cols-12 mt-4 gap-y-8 gap-x-6 items-center relative">
-		<!-- <div class="order-2 lg:order-1 col-start-1 xl:col-start-2 col-end-12 lg:col-end-6 overflow-visible"> -->
 		<div class="order-2 lg:order-1 col-start-1 xl:col-start-2 lg:col-end-6 xl:col-end-6 lg:col-span-6 xl:col-span-5 overflow-visible">
 			<p class="lg:mt-12 ml-12 lg:ml-0">A healthy community is a thriving one. Early access and preventative care is key to driving the wellbeing of our communities. Everyone deserves equal access to comprehensive health coverage.</p>
 			<div class="
@@ -382,8 +393,13 @@ get_header(); ?>
 				lg:after:ml-0">
 			</div>
 		</div>
-		<div class="order-1 lg:order-2 lg:col-start-7 xl:col-start-7 lg:col-span-6 xl:col-span-5">
+		<div class="order-1 lg:order-2 lg:col-start-7 xl:col-start-7 lg:col-span-6 xl:col-span-5 relative">
 			<img src="//via.placeholder.com/520x520" alt="" class="mx-auto rounded-4xl w-full h-auto">
+			<img 
+				src="https://via.placeholder.com/94x94" 
+				alt="Placeholder Image"
+				class="absolute right-6 -bottom-6 w-1/3 lg:bottom-12 lg:-right-12"
+			>
 		</div>
 	</div>
 </div>
